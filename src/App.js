a9fff29ad738225bc76f90548ac7a6e7a55f9236
VISHNU LANJHI <suvish.kgp@gmail.com>
2023-06-26 01:02:33 +0530

    added file directly
    basic setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Namaste!! It's <code>SUV1SH's Blog</code> Comming Soon...
+        </p>
+        <a
+          className="App-link"
+          href="https://twitter.com/suv1sh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Connect SUViSH.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
